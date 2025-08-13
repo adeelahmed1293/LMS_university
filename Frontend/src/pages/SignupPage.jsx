ef521feef586a050da1bab71a87baed290fdc856
adeelahmed1293 <154768025+adeelahmed1293@users.noreply.github.com>
2025-08-13 10:53:15 +0500

    Update SignupPage.jsx

--- Frontend/src/pages/SignupPage.jsx
+++ Frontend/src/pages/SignupPage.jsx
@@ -67,7 +67,7 @@ const SignupPage = () => {
     }
   
     try {
-      const response = await fetch("https://lms-university-backend.vercel.app/user/signup", {
+      const response = await fetch("https://lms-university-theta.vercel.app/user/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
